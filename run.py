da486b99a0c693396ef8af7974f059d2b4cd92d1
ftbyu <ftb.y.m.kashima.07@gmail.com>
2021-05-06 03:16:14 +0900

    [Add] 各ルートにおける挙動確認

--- run.py
+++ run.py
@@ -1,9 +1,25 @@
 from flask import Flask, request
 app = Flask(__name__)
 
+@app.route('/test', methods=['GET', 'POST'])
+def post_test():
+    if request.method == 'GET':
+        pass
+    else:
+        pass
+    return '0'
+
 @app.route("/")
 def hello_world():
   return "Hello World!"
+
+@app.route("/flask")
+def hello_sample():
+  return "Hello Flask."
+
+@app.route("/user/<user_id>")
+def hello_person(user_id):
+  return "Hello " + user_id
 
 if  __name__=='__main__':
   app.run()
