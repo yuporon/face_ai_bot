2297e43fe5145ae52918add98d3e642d258558b5
ftbyu <ftb.y.m.kashima.07@gmail.com>
2021-05-07 20:51:33 +0900

    [Add] オードリヘップバーンの顔認証機能を追加

--- app.py
+++ app.py
@@ -28,6 +28,9 @@ face_client = FaceClient(
 	CognitiveServicesCredentials(YOUR_FACE_API_KEY)
 )
 
+PERSON_GROUP_ID = os.getenv('PERSON_GROUP_ID')
+PERSON_ID_AUDREY = os.getenv('PERSON_ID_AUDREY')
+
 @app.route("/callback", methods=['POST'])
 def callback():
 	# get X-Line-Signature header value
@@ -69,11 +72,25 @@ def handle_image(event):
 		print(detected_faces)
 		# 検出結果に応じて処理を分ける
 		if detected_faces != []:
-			# 検出された顔の最初のIDを取得
-			text = detected_faces[0].face_id
+			# 顔検出ができたら顔認証を行う
+			valified = face_client.face.verify_face_to_person(
+				face_id = detected_faces[0].face_id,
+				person_group_id = PERSON_GROUP_ID,
+				person_id = PERSON_ID_AUDREY
+			)
+			# 認証結果に応じて処理を変える
+			if valified:
+				if valified.is_identical:
+					# 顔認証が一致した場合（スコアもつけて返す）
+					text = 'この写真はオードリーヘップバーンです(score:{:.3f})'.format(valified.confidence)
+				else:
+					# 顔認証が一致した場合（スコアもつけて返す）
+					text = 'この写真はオードリーヘップバーンではありません(score:{:.3f})'.format(valified.confidence)
+			else:
+				text = '識別できませんでした。'
 		else:
 			# 検出されない場合のメッセージ
-			text = "no faces detected"
+			text = "写真から顔が検出できませんでした。他の画像で試してください。"
 	except:
 		# エラー時のメッセージ
 		text = "error" 
