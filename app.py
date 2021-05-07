120f9cff230bea040159ad6e23cfed90eb3cc928
yuporon <ftb.y.m.kashima.07@gmail.com>
2021-05-08 03:21:34 +0900

    [Fix] 修正&検証

--- app.py
+++ app.py
@@ -70,11 +70,25 @@ def handle_image(event):
         print(detected_faces)
         # 検出結果に応じて処理を分ける
         if detected_faces != []:
-            # 検出された顔の最初のIDを取得
-            text = detected_faces[0].face_id
+            # 顔検出ができたら顔認証を行う
+            valified = face_client.face.verify_face_to_person(
+                face_id = detected_faces[0].face_id,
+                person_group_id = PERSON_GROUP_ID,
+                person_id = PERSON_ID_YOSIZAWA
+            )
+            # 認証結果に応じて処理を変える
+            if valified:
+                if valified.is_identical:
+                    # 顔認証が一致した場合（スコアもつけて返す）
+                    text = 'スコア{:.3f}\nあなたは吉沢亮ですね'.format(valified.confidence)
+                else:
+                    # 顔認証が一致した場合（スコアもつけて返す）
+                    text = 'スコア{:.3f}\nあなたは吉沢亮ではないですね'.format(valified.confidence)
+            else:
+                text = '識別できませんでした。'
         else:
             # 検出されない場合のメッセージ
-            text = "no faces detected"
+            text = "写真から顔が検出できませんでした。他の画像で試してください。"
     except:
         # エラー時のメッセージ
         text = "error!!" 
