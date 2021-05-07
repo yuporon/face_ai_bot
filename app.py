1e884f5fe0cd2366afdf7c6daacb742fb48f26d7
ftbyu <ftb.y.m.kashima.07@gmail.com>
2021-05-07 20:36:20 +0900

    [Add] 顔認証機能を追加

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ import os
 from flask import Flask, request, abort
 
 from linebot import (
-    LineBotApi, WebhookHandler
+  LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+  InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageMessage
+  MessageEvent, TextMessage, TextSendMessage, ImageMessage
 )
+from io import BytesIO
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
 
 app = Flask(__name__)
 
@@ -18,41 +21,67 @@ YOUR_CHANNEL_SECRET = os.getenv('YOUR_CHANNEL_SECRET')
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+YOUR_FACE_API_KEY = os.environ["YOUR_FACE_API_KEY"]
+YOUR_FACE_API_ENDPOINT = os.environ["YOUR_FACE_API_ENDPOINT"]
+face_client = FaceClient(
+	YOUR_FACE_API_ENDPOINT,
+	CognitiveServicesCredentials(YOUR_FACE_API_KEY)
+)
+
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+	# get X-Line-Signature header value
+	signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+	# get request body as text
+	body = request.get_data(as_text=True)
+	app.logger.info("Request body: " + body)
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+	# handle webhook body
+	try:
+		handler.handle(body, signature)
+	except InvalidSignatureError:
+		print("Invalid signature. Please check your channel access token/channel secret.")
+		abort(400)
 
-    return 'OK'
+	return 'OK'
 
 # textへの返信
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+	line_bot_api.reply_message(
+		event.reply_token,
+		TextSendMessage(text=event.message.text))
 
 # 画像への返信
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text='画像です')
-    )
+	try:
+		# メッセージIDを受け取る
+		message_id = event.message.id
+		# メッセージIDに含まれるmessage_contentを抽出する
+		message_content = line_bot_api.get_message_content(message_id)
+		# contentの画像データをバイナリデータとして扱えるようにする
+		image = BytesIO(message_content.content)
+		
+		# Detect from streamで顔検出
+		detected_faces = face_client.face.detect_with_stream(image)
+		print(detected_faces)
+		# 検出結果に応じて処理を分ける
+		if detected_faces != []:
+			# 検出された顔の最初のIDを取得
+			text = detected_faces[0].face_id
+		else:
+			# 検出されない場合のメッセージ
+			text = "no faces detected"
+	except:
+		# エラー時のメッセージ
+		text = "error" 
+	# LINEチャネルを通じてメッセージを返答
+	line_bot_api.reply_message(
+		event.reply_token,       
+		TextSendMessage(text=text)
+	)
 
 if __name__ == "__main__":
-    app.run()
-
-if __name__ == "__main__":
-    app.run()
+	app.run()
