fbf74fbdeadefa555e9e7bef2655ab3e5af65d87
ftbyu <ftb.y.m.kashima.07@gmail.com>
2021-05-07 23:09:17 +0900

    [Fix] 戻して検証

--- app.py
+++ app.py
@@ -58,41 +58,12 @@ def handle_message(event):
 # 画像への返信
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image(event):
-	try:
-		# メッセージIDを受け取る
-		message_id = event.message.id
-		# メッセージIDに含まれるmessage_contentを抽出する
-		message_content = line_bot_api.get_message_content(message_id)
-		# contentの画像データをバイナリデータとして扱えるようにする
-		image = BytesIO(message_content.content)
-		
-		# Detect from streamで顔検出
-		detected_faces = face_client.face.detect_with_stream(image)
-		print(detected_faces)
-		# 検出結果に応じて処理を分ける
-		if detected_faces != []:
-			# 顔検出ができたら顔認証を行う
-			valified = face_client.face.verify_face_to_person(
-				face_id = detected_faces[0].face_id,
-				person_group_id = PERSON_GROUP_ID,
-				person_id = PERSON_ID_AUDREY
-			)
-			# 認証結果に応じて処理を変える
-			if valified:
-				if valified.is_identical:
-					# 顔認証が一致した場合（スコアもつけて返す）
-					text = '{:.3f*100}％！！\nあなたは吉沢亮様！？'.format(valified.confidence)
-				else:
-					# 顔認証が一致した場合（スコアもつけて返す）
-					text = '{:.3f*100}％？？\n吉沢亮様じゃないね。。。'.format(valified.confidence)
-			else:
-				text = '識別できませんでした。'
-		else:
-			# 検出されない場合のメッセージ
-			text = "写真から顔が検出できませんでした。他の画像で試してください。"
-	except:
-		# エラー時のメッセージ
-		text = "error!" 
+  if detected_faces != []:
+		# 検出された顔の最初のIDを取得
+		text = detected_faces[0].face_id
+	else:
+		# 検出されない場合のメッセージ
+		text = "no faces detected"
 	# LINEチャネルを通じてメッセージを返答
 	line_bot_api.reply_message(
 		event.reply_token,       
