e25eaebf8413cc8dad79d2f398e6582b06cfcfd7
ftbyu <ftb.y.m.kashima.07@gmail.com>
2021-05-07 18:16:03 +0900

    [Add] 画像に対して返信するように

--- app.py
+++ app.py
@@ -36,6 +36,14 @@ def callback():
 
     return 'OK'
 
+# textへの返信
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+# 画像への返信
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image(event):
     line_bot_api.reply_message(
